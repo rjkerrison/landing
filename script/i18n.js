b1ddee1ec42df21557f669df9b36043e166bff32
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-08-05 13:50:38 +0100

    i18n allowing for keeping secondary preference

--- script/i18n.js
+++ script/i18n.js
@@ -15,32 +15,48 @@ function getCurrentLang() {
   return document.querySelector('meta[lang]').getAttribute('lang')
 }
 
-function getPreferredLocale() {
+function getPreferredLocale(current) {
   const userLocales = [
     ...window.navigator.languages,
     window.navigator.language,
     window.navigator.userLanguage,
   ]
+  if (userLocales.some((x) => x === current)) {
+    return tryGetMatch(current)
+  }
 
   for (let i = 0; i < userLocales.length; i++) {
     const locale = userLocales[i]
-    for (let key of Object.keys(availableLocales)) {
-      const spec = availableLocales[key]
-      if (spec.locale === locale) {
-        return spec
-      }
-      if (spec.alternativeLocales.some((x) => x === locale)) {
-        return spec
-      }
+    const match = tryGetMatch(locale)
+    if (match) {
+      return match
     }
   }
   // Default to English
   return availableLocales.en
 }
 
+function tryGetMatch(localeCode) {
+  for (let key of Object.keys(availableLocales)) {
+    const spec = availableLocales[key]
+    if (isMatchingLocale(spec, localeCode)) {
+      return spec
+    }
+  }
+  return null
+}
+
+function isMatchingLocale(availableLocale, localeCode) {
+  const isPerfectMatch = availableLocale.locale === localeCode
+  const isCloseMatch = availableLocale.alternativeLocales.some(
+    (x) => x === localeCode
+  )
+  return isPerfectMatch || isCloseMatch
+}
+
 function redirectIfOtherLocalePreferred() {
   const currentLang = getCurrentLang()
-  const locale = getPreferredLocale()
+  const locale = getPreferredLocale(currentLang)
   if (currentLang === locale.locale) {
     return
   }
